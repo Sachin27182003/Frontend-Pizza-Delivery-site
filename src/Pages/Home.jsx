1de216b3e91b4114a1753ad219ba7c14910fe2ee
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-02-20 03:32:43 +0530

    Update 0.2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,54 @@
 import { ArrowRightLongLine } from "../Components/Icons/Arrow-right-long-line";
+import pizzaImage from '../assets/Images/pizza2.png';
 
 function Home(){
 
     return (
         <div>
             {/* Hero Section */}
-        <section className="flex flex-col-reverse items-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300">
+        <section className="flex flex-col-reverse items-center justify-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300">
                                                                {/* justify-center */}
-            <div className="w-4/6 ml-[400px] text-center whitespace-nowrap md:w-2/6 md:text-left ">
+            <div className="w-4/6 ml-4 text-center whitespace-nowrap md:w-2/6 md:text-left ">
             
                 <div className="flex justify-center text-4xl md:justify-normal">
                 
                     <h1 className="pb-5 font-bold text-transparent bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text ">
-                        Welcome to the Pizza Delivery site {'...'}
+                        Enjoy the slice {'...'}
                     </h1>
                     <h1>
                         😋
                     </h1>
                 </div>
-                <p className=" ml-2 pb-4 text-[#6B7280] ">
+                <p className=" pb-4 text-[#6B7280] ">
                         The Pizza site lets you order your favourite pizza from the comfort of your home. <br/>
-                        Enjoy the best pizza in your town with just one click!
+                        {" "}Enjoy the best pizza in your town with just one click!
                 </p>
                 <button className="flex items-center px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 group">
                     Order Now !
-                    {/* https://www.reshot.com/free-svg-icons/item/right-arrow-UCA8NGYZDJ/ */}
+                    {/* https://reactsvgicons.com/search?q=arrow */}
                     <span className="inline-block ml-2 transition-transform ease-in-out group-hover:translate-x-2">
                         <ArrowRightLongLine/>
                     </span>
                 </button>
             </div> 
+
+            <div>
+                <img
+                    src={pizzaImage}
+                    alt="pizza"
+                    height={550}
+                    width={400}
+                />
+            </div>
+
+        </section>
+
+        {/* Services ssection */}
+        <section className="py-4 mt-6 bg-gradient-to-r from-amber-50 to-orange-300">
+
+            <div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
+
+            </div>
 
         </section>
         </div>
