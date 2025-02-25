ce5628ef837efa2e63cbb86e34636188a8557596
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-02-26 00:39:01 +0530

    Add some extra features

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import { ArrowRightLongLine } from "../Components/Icons/Arrow-right-long-line";
 import pizzaImage from '../assets/Images/pizza2.png';
+import cookingImage from '../assets/Images/cooking1.png';
+import { PatchCheck } from "../Components/Icons/patch-check";
 
 function Home(){
 
@@ -45,11 +47,47 @@ function Home(){
 
         {/* Services ssection */}
         <section className="py-4 mt-6 bg-gradient-to-r from-amber-50 to-orange-300">
+           <div className="container flex flex-col md:flex-row">
+                <div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
+                    <img
+                    src={cookingImage}
+                    width={500}
+                    className="rounded-lg"
+                    alt="Cooking"
+                    />
+                </div>
+                <div className="flex flex-col flex-wrap text-center lg:py-6 lg:w-1/2 lg:pl-12 lg:text-left">
+                    <div className="flex flex-col items-center lg:items-start" >
+                        <div>
+                        <h2 className="mb-2 text-5xl font-extrabold text-transparent title-font bg-gradient-to-r from-orange-600 to-orange-400 bg-clip-text">
+                            Cooked by the best chefs in the World !
+                        </h2>
+                        <p>
+                            There are many benefits regarding to that but the main ones are:
+                        </p>
+                        </div>
+                    </div>
 
-            <div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
-
+                    <div className="w-full p-1" >
+                        <div className="flex items-center h-full p-2 pl-0 text-2xl rounded">
+                        <PatchCheck className="text-base leading-relaxed text-[#6B7280] h-9 w-9 mr-2" />
+                        <span> Perfect taste</span>
+                        </div>
+                    </div>
+                    <div className="w-full p-1" >
+                        <div className="flex items-center h-full p-2 pl-0 text-2xl rounded">
+                        <PatchCheck className="text-base leading-relaxed text-[#6B7280] h-9 w-9 mr-2" />
+                        <span> Prepared Quickly</span>
+                        </div>
+                    </div>
+                    <div className="w-full p-1" >
+                        <div className="flex items-center h-full p-2 pl-0 text-2xl rounded">
+                        <PatchCheck className="text-base leading-relaxed text-[#6B7280] h-9 w-9 mr-2" />
+                        <span>Hygenic food guaranteed</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </section>
         </div>
     )
